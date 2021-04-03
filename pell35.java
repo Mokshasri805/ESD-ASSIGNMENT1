10004eeece48c560c5a54485f00efd30d58dc986
Mokshasri805 <75661387+Mokshasri805@users.noreply.github.com>
2021-04-03 17:54:28 +0530

    first commit

---
+++ pell35.java
@@ -0,0 +1,16 @@
+ //Implement a Java program to print the first 15 numbers of the Pell series
+ class pellseries  {
+
+    public static void main(String args[])
+    {
+	int n,a=1,b=0,c;
+    System.out.println("First 15 Pell numbers: ");
+    for(n=1; n<=15; n++)
+     {
+      c= a + 2*b;
+      System.out.print(c+" ");
+      a = b;
+      b = c;
+     }
+   }
+ }
